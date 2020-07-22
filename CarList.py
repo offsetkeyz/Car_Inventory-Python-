ed1304f7e624627ebbcdcc41ae174fad30f2563f
Colin <colin.mcallister89@yahoo.com>
2020-07-21 21:07:35 -0700

    second commit, nothing of note

--- CarList.py
+++ CarList.py
@@ -3,15 +3,24 @@ from Car import *
 
 class CarList:
 
-    def __init__(self, list_of_cars):
+    def __init__(self, date, list_of_cars):
 
+        # the date acts as the name of the list
+        self.date = date
         self.list_of_cars = list_of_cars
 
     def find_car(self, stock_number):
         for car in self.list_of_cars:
             if type(car) is Car:
                 if stock_number == car.stock_number:
-                    return car
+                    return f"{car} was shot on {self.date}" # TODO maybe make this boolean
             else:
                 return "This is not a car"
         return "Stock number not found"
+
+    # Returns all cars in list to String
+    def __str__(self) -> str:
+        blurb = ""
+        for car in self.list_of_cars:
+            blurb += car.__str__() + "\n"
+        return blurb
--- main.py
+++ main.py
@@ -5,5 +5,14 @@ firstList = [Car("P3456", "Findlay", "Honda", "Accord", 2020, 15, "new"),
              Car("P3446", "Findlay", "Honda", "Accord", 2020, 15, "new"),
              Car("P3457", "Findlay", "Honda", "Accord", 2020, 15, "new")]
 
-actual_list = CarList(firstList)
+actual_list = CarList("Dec 7, 1941", firstList)
+list_two = CarList("Dec 6, 2020", firstList)
+
+list_of_lists = [actual_list, list_two]
+for l in list_of_lists:
+    print(l.find_car("P3456"))
+
+print(list_of_lists)
+
 print(actual_list.find_car("P3445"))
+print(actual_list)
